eddb9deb6dcb4e7a4bc913c9dedb6dc631dd1142
Devin Polichetti <devinpolichetti@gmail.com>
2023-03-22 15:23:28 -0400

    added templates for homepage, login, sign up

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,20 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 let sequelize;
-
+console.log(process.env);
 if (process.env.JAWSDB_URL) {
-	sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-	sequelize = new Sequelize(
-		process.env.DB_NAME,
-		process.env.DB_USER,
-		process.env.DB_PASSWORD,
-		{
-			host: "127.0.0.1",
-			dialect: "mysql",
-			port: 3306,
-		}
-	);
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "127.0.0.1",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
 }
 
 module.exports = sequelize;
