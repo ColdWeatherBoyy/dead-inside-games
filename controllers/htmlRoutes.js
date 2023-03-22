63717597523ed3b215bdeb1289767b65c23209a5
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-03-22 10:13:23 -0400

    heroku deployment wildcard route

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
 
+router.get("*", async (req, res) => {
+	try {
+		res.render("helloworld", {});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
