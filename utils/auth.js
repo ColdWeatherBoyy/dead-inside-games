dfc8ef6e27db61cc008bdf9dfc6da61f5b893221
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-03-22 12:30:57 -0400

    updates withAuth to redirect homepage to /minesweeper if logged in and adds the get route for highscores in htmlRoutes (to be included in the minesweeper html route)

--- utils/auth.js
+++ utils/auth.js
@@ -2,8 +2,13 @@ const withAuth = (req, res, next) => {
 	if (!req.session.loggedIn) {
 		console.log(req.session);
 		res.redirect("/login");
+		// if logged in, should redirect homepage to /minesweeper, or to go to any other legal page
 	} else {
-		next();
+		if (req.url === "/") {
+			res.redirect("/minesweeper");
+		} else {
+			next();
+		}
 	}
 };
 
