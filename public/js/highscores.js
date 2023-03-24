9e2e50a02c1bb80cf2240503dc6bcceabfa6d56b
dmantilla17 <derekaraujo11@gmail.com>
2023-03-24 12:39:33 -0400

    logic for highscores modal

--- public/js/highscores.js
+++ public/js/highscores.js
@@ -1,1 +1,28 @@
+const highscoreButton = document.querySelectorAll("[data-modal-target]");
+const closeButton = document.querySelector("#close-button");
+const overlay = document.getElementById("overlay");
 
+highscoreButton.forEach((button) => {
+  button.addEventListener(" click ", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+closeButton.forEach((button) => {
+  button.addEventListener(" click ", () => {
+    const modal = document.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
