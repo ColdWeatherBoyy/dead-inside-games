7247ee0769d09f45cc2d6ad0051fdff52554017b
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-03-24 13:48:53 -0400

    adds logic for terms and conditions (and an easter egg)

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,8 +3,9 @@ const signupFormHandler = async (event) => {
 
 	const username = document.querySelector("#username").value.trim();
 	const password = document.querySelector("#password").value.trim();
+	const checked = document.querySelector("#checkbox").checked;
 
-	if (username && password) {
+	if (username && password && checked === true) {
 		console.log(username);
 		const response = await fetch("/api/users/signup", {
 			method: "POST",
@@ -15,7 +16,7 @@ const signupFormHandler = async (event) => {
 		const data = await response.json();
 
 		if (response.ok) {
-			document.location.replace("/minesweeper");
+			// document.location.replace("/minesweeper");
 		} else if (data === "Username is not unique.") {
 			alert("Username is not unique, please try again!");
 		} else if (data === "Password does not meet constraints.") {
@@ -24,7 +25,9 @@ const signupFormHandler = async (event) => {
 			);
 		}
 	} else {
-		alert("You've gotta enter a username and password!");
+		alert(
+			"You've gotta enter a username and password and accept the terms and conditions!!"
+		);
 	}
 };
 
